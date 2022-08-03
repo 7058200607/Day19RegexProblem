47cdf6ed0316d621314fac9533e5b19398790d34
Vrushali Joshi <vrushalijoshi589@gmail.com>
2022-08-03 22:19:32 +0530

    As a User need to

---
+++ Day19RegexProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Day19RegexProblem
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Regestration Using Regex");
+            UserRegestration userRegestration = new UserRegestration();
+            Console.WriteLine("Enter Your First Name :");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine(userRegestration.ValidateFirstName(FirstName));
+        }
+    }
+}
---
+++ Day19RegexProblem/UserRegestration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Day19RegexProblem
+{
+    public class UserRegestration
+    {
+        public const string Regex_FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, Regex_FirstName);
+        }
+    }
+}
+
+
